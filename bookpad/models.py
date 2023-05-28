eedcab3a2773fea5c05ffa0ada813a02a30acca1
belem <graceitamunoala@gmail.com>
2023-05-28 22:22:24 +0100

    commit

--- bookpad/models.py
+++ bookpad/models.py
@@ -3,20 +3,29 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    pass
+    username = models.CharField(max_length=100, on_delete=models.CASCADE)
+    password = models.CharField(max_length=30, blank=False)
+    email = models.EmailField(max_length=100)
+    bio = models.CharField(max_length=400)
+    post = models.ImageField()
 
 
-class Feed(models.Model):
-    pass
+    def __str__(self):
+        return self.username
+
 
 
 class UserProfile(models.Model):
     pass
 
 
+
 class Post(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    imagebox = models.ImageField()
+    caption = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Like(models.Model):
-    pass
+
