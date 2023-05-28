9e40ccc86f07ccb556aab23fe8c502460737b41e
belem <graceitamunoala@gmail.com>
2023-05-29 00:06:12 +0100

    commit

--- BookPadProj/settings.py
+++ BookPadProj/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'BookPadProj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['bookpad/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- bookpad/views.py
+++ bookpad/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import bcrypt
+from cryptography.fernet import Fernet
+from .models import User
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        phone = request.POST['phone']
+
+
+         # Generate encryption key
+        encryption_key = Fernet.generate_key()
+        cipher = Fernet(encryption_key)
+
+        # Encrypt the password
+        encrypted_password = cipher.encrypt(password.encode('utf-8'))
+
+        # Hash the encrypted password
+        hashed_password = bcrypt.hashpw(encrypted_password, bcrypt.gensalt())
+       
+       
+        #create a user
+        user = User.objects.create(
+            username=username,
+            password=hashed_password,
+            email=email,
+            phone=phone
+        )
+        user.save()
+        return redirect('login')
+
+    return render(request, 'signup.html')    
+        
+
+def login(request):
+    pass
+
