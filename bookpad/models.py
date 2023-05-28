03bbbd64cb80bd2f5359a04532ac81514691710d
belem <graceitamunoala@gmail.com>
2023-05-28 22:37:14 +0100

    updating the models

--- bookpad/models.py
+++ bookpad/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     username = models.CharField(max_length=100, on_delete=models.CASCADE)
     password = models.CharField(max_length=30, blank=False)
     email = models.EmailField(max_length=100)
@@ -23,9 +25,17 @@ class UserProfile(models.Model):
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    imagebox = models.ImageField()
+    image_url = models.CharField(max_length=200, blank=True)
     caption = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    # class Meta:
+    #     verbose_name: "post"
+    #     verbose_name_plural = "Posts"
+
 
 
 
