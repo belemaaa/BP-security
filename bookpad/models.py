7364c5e55611adf34a4859809505c4c9dab80963
belem <graceitamunoala@gmail.com>
2023-05-28 21:10:20 +0100

    initial commit

---
+++ bookpad/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    pass
+
+
+class Feed(models.Model):
+    pass
+
+
+class UserProfile(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class Like(models.Model):
+    pass
